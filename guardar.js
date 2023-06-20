44ff2ac384405e9c5e79dc77f31083d8ebdc7a69
hazahel117 <105388255+hazahel117@users.noreply.github.com>
2023-06-19 19:07:05 -0600

    Add files via upload

--- guardar.js
+++ guardar.js
@@ -1,22 +1,42 @@
+document.getElementById("formulario").addEventListener("submit", function(event){
+    event.preventDefault();
+    guardar();
+});
+
+
 function guardar(){
-    db.collection("users").add({
-        nombre: document.getElementById("nombre").value,
-        email: document.getElementById("email").value,
-        telefono: document.getElementById("telefono").value,
-        edad: document.getElementById("edad").value,
-        casado: document.getElementById("casado").value,
-        hijos: document.getElementById("hijos").value,
-        titulado: document.getElementById("titulado").value,
-        especialidad: document.getElementById("especialidades").value,
-        expectativa_economica: document.getElementById("price").value
-    })
-    .then((docRef) => {
-        alert("Registro exitoso")
-    })
-    .catch((error) => {
-        console.error("Error adding document: ", error);
-        alert("Ocurrió un error en el registro")
-    });
+    var ver_tel = parseInt(document.getElementById("telefono").value)
+    var ver_edad = parseInt(document.getElementById("edad").value)
+
+    if (document.getElementById("nombre").value
+        && document.getElementById("email").value
+        && document.getElementById("telefono").value && !isNaN(ver_tel)
+        && document.getElementById("edad").value && !isNaN(ver_edad)){
+            db.collection("users").add({
+                nombre: document.getElementById("nombre").value,
+                email: document.getElementById("email").value,
+                telefono: document.getElementById("telefono").value,
+                edad: document.getElementById("edad").value,
+                casado: document.getElementById("casado").value,
+                hijos: document.getElementById("hijos").value,
+                titulado: document.getElementById("titulado").value,
+                especialidad: document.getElementById("especialidades").value,
+                expectativa_economica: document.getElementById("price").value,
+                años_experiencia: document.getElementById("experiencia").value
+            })
+            .then((docRef) => {
+                alert("Registro exitoso")
+                document.getElementById("formulario").reset();
+                window.location.href = "#";
+            })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+                alert("Ocurrió un error en el registro")
+            });
+        } else{
+            alert("Información inválida")
+        }
+    
 }
 
 
--- ver.js
+++ ver.js
@@ -1,4 +1,51 @@
+/*var container = document.getElementById("container");
+
+
+
+var datos = [
+    { nombre: "John Doe", correo: "john@example.com" },
+    { nombre: "Jane Smith", correo: "jane@example.com" },
+    // Agrega más datos según sea necesario
+  ];
+*/
+
+var columnas = ['nombre', 'email', 'telefono','edad','casado','hijos','titulado','años_experiencia'];
+
+db.collection("users").get().then(function(querySnapshot){
+    querySnapshot.forEach(function(doc){
+        var datos = doc.data();
+        mostrar(datos);
+    });
+});
+
+function mostrar(datos){
+    var tabla = document.getElementById('tablaDatos');
+    var fila = tabla.insertRow();
+
+    for (var i = 0; i < columnas.length; i++) {
+        var columna = columnas[i];
+        var celda = fila.insertCell();
+        celda.innerHTML = datos[columna];
+    }
+
+    /*for (var key in datos){
+        if(datos.hasOwnProperty(key)){
+            var celda = fila.insertCell();
+            celda.innerHTML = datos[key];
+        }
+    }*/
+    db.collection("users").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    });
+}
+
 function ver(){
+    container.style.display ="flex";
+
+    /*
     db.collection("users").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             document.getElementById("mostrar-nombre").innerHTML=`${doc.data().nombre}`
@@ -8,6 +55,7 @@ function ver(){
             document.getElementById("mostrar-hijos").innerHTML=`${doc.data().hijos}`
             document.getElementById("mostrar-casado").innerHTML=`${doc.data().casado}`
             document.getElementById("mostrar-titulado").innerHTML=`${doc.data().titulado}`
+            document.getElementById("mostrar-experiencia").innerHTML=`${doc.data().años_experiencia}`
         });
-    });
+    });*/
 }
