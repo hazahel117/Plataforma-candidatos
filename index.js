fa4726578d26d6c3b8498665b5ea227de3697537
hazahel117 <105388255+hazahel117@users.noreply.github.com>
2023-07-03 18:59:55 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const price = document.querySelector('#price')
-const output = document.querySelector('.price-output')
+document.addEventListener('DOMContentLoaded', function() {
+            
+  let container = document.getElementById('container');
+  let html=''
+  db.collection("vacantes_disponibles").orderBy("orden", "asc").get().then(function(querySnapshot){
+      querySnapshot.forEach(function(doc){
+          var datos = doc.data();
+          console.log(datos.nombre);
+          if  (datos.orden == '1'){
+            html +=`<div class='vacante orden1'>
+                  <label>${datos.nombre}</label>
+              </div>`;
+          }else if (datos.orden == '2'){
+          html +=`<div class='vacante orden2'>
+                  <label>${datos.nombre}</label>
+              </div>`;
+          }else if (datos.orden == '3'){
+            html +=`<div class='vacante orden3'>
+                    <label>${datos.nombre}</label>
+                </div>`;
+            }
+      });
+      container.innerHTML = html;
+      var buttons = document.getElementsByClassName('vacante');
 
-output.textContent = price.value
+      var redirectToLink = function() {
+          window.location.href = 'form.html';
+          
+      };
 
-price.addEventListener('input', function() {
-  output.textContent = price.value
+      for (var i = 0; i < buttons.length; i++) {
+          buttons[i].addEventListener('click', redirectToLink);
+      }
+  });
 });
-
 
